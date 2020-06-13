38a66600194f7cfebb765dcf6ce16eb8682e36b0
Matthew T. Popich <thehailo@users.noreply.github.com>
2020-06-13 00:44:57 -0400

    loops.py (Initial commit.)

---
+++ loops.py
@@ -0,0 +1,15 @@
+# Count to 10 using a WHILE LOOP.
+i = 1
+while i <= 10:
+	print(i)
+	i += 1
+
+# Count to 20 using a FOR LOOP.
+for i in range(11,20):
+	print(i)
+
+# Count to 100 by 10's.
+for i in range(20,101,10):
+	print(i)
+else:
+	print("Now go away.")
